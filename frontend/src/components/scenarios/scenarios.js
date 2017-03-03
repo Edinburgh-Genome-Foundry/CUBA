f2b7415c18ebeb6037824ac2c8bacd06a71776e5
Zulko <valentin.zulkower@gmail.com>
2017-03-03 11:01:26 +0000

    Display only two scenarios at the moment

--- frontend/src/components/scenarios/scenarios.js
+++ frontend/src/components/scenarios/scenarios.js
@@ -1,15 +1,6 @@
-import PredictDigests from './PredictDigests'
-import SimulateGGAssemblies from './SimulateGGAssemblies'
-import SculptASequence from './SculptASequence'
-import SelectDigestion from './SelectDigestion'
-import SeparateConstructs from './SeparateConstructs'
-
 export default {
   list: [
-    PredictDigests,
-    SelectDigestion,
-    SeparateConstructs,
-    SimulateGGAssemblies,
-    SculptASequence
+    require('./PredictDigests'),
+    require('./SimulateGGAssemblies')
   ]
 }
