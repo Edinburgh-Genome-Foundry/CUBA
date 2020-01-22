772803c24f167ff65b34afbe7b25a3a86833bd9b
Zulko <valentin.zulkower@gmail.com>
2020-01-22 16:12:28 +0000

    black formatting in select_digestions_view

--- backend/app/views/select_digestions/SelectDigestionsView.py
+++ backend/app/views/select_digestions/SelectDigestionsView.py
@@ -3,12 +3,17 @@
 from rest_framework import serializers
 from collections import OrderedDict
 from ..base import AsyncWorker, StartJobView
-from ..tools import (records_from_data_files,
-                     data_to_html_data,
-                     set_record_topology,
-                     matplotlib_figure_to_svg_base64_data)
-from bandwitch import (IdealDigestionsProblem, SeparatingDigestionsProblem,
-                       LADDERS)
+from ..tools import (
+    records_from_data_files,
+    data_to_html_data,
+    set_record_topology,
+    matplotlib_figure_to_svg_base64_data,
+)
+from bandwitch import (
+    IdealDigestionsProblem,
+    SeparatingDigestionsProblem,
+    LADDERS,
+)
 import bandwagon
 import flametree
 from bandwagon import plot_records_digestions
@@ -16,13 +21,17 @@ from io import BytesIO
 
 
 digestion = serializers.ListField(child=serializers.CharField())
+
+
 class FileSerializer(serializers.Serializer):
     name = serializers.CharField()
     content = serializers.CharField()
 
+
 class ComaSeparatedList(serializers.CharField):
     def to_representation(self, value):
         return [e.strip() for e in value.split(",")]
+
 
 class serializer_class(serializers.Serializer):
     goal = serializers.CharField()
@@ -37,11 +46,11 @@ class serializer_class(serializers.Serializer):
     plot_cuts = serializers.BooleanField()
     topology = serializers.CharField()
 
-class worker_class(AsyncWorker):
 
+class worker_class(AsyncWorker):
     def work(self):
 
-        self.logger(message='Exploring possible digestions...')
+        self.logger(message="Exploring possible digestions...")
 
         data = self.data
         ladder = LADDERS[data.ladder]
@@ -49,42 +58,48 @@ class worker_class(AsyncWorker):
         records = records_from_data_files(data.files)
         for record in records:
             set_record_topology(record, data.topology)
-        sequences = OrderedDict([
-            (record.id, str(record.seq))
-            for record in records
-        ])
+        sequences = OrderedDict(
+            [(record.id, str(record.seq)) for record in records]
+        )
 
         self.logger(message="Initializing...")
 
-        if (data.goal == 'ideal'):
+        if data.goal == "ideal":
             mini, maxi = data.bands_range
             problem = IdealDigestionsProblem(
-                sequences=records, enzymes=enzymes, ladder=ladder,
-                topology='auto',
-                min_bands=mini, max_bands=maxi,
-                max_enzymes_per_digestion=data.max_enzymes
+                sequences=records,
+                enzymes=enzymes,
+                ladder=ladder,
+                topology="auto",
+                min_bands=mini,
+                max_bands=maxi,
+                max_enzymes_per_digestion=data.max_enzymes,
             )
         else:
             problem = SeparatingDigestionsProblem(
-                 sequences=sequences, enzymes=enzymes, ladder=ladder,
-                 max_enzymes_per_digestion=data.max_enzymes
+                sequences=sequences,
+                enzymes=enzymes,
+                ladder=ladder,
+                max_enzymes_per_digestion=data.max_enzymes,
             )
-        self.logger(message='Selecting digestions...')
+        self.logger(message="Selecting digestions...")
         score, selected_digestions = problem.select_digestions(
-            max_digestions=data.max_digestions, search='full')
-        bands_props = None if not data.show_bands_sizes else dict(
-            label='=size',
-            label_fontdict=dict(size=6)
+            max_digestions=data.max_digestions, search="full"
+        )
+        bands_props = (
+            None
+            if not data.show_bands_sizes
+            else dict(label="=size", label_fontdict=dict(size=6))
         )
         axes = problem.plot_digestions(
             selected_digestions,
-            patterns_props={'label_fontdict': {'rotation': 35}},
-            bands_props=bands_props
+            patterns_props={"label_fontdict": {"rotation": 35}},
+            bands_props=bands_props,
         )
         figure_data = matplotlib_figure_to_svg_base64_data(
-            axes[0].figure, bbox_inches='tight'
+            axes[0].figure, bbox_inches="tight"
         )
-        
+
         if data.plot_cuts:
             ladder = bandwagon.custom_ladder(None, ladder.bands)
             self.logger(message="Plotting cuts maps...")
@@ -96,19 +111,20 @@ class worker_class(AsyncWorker):
                     (rec, digestion)
                     for rec in records
                     for digestion in selected_digestions
-                ]
+                ],
             )
-            pdf_data = zip_root["Details.pdf"].read('rb')
-            pdf_data = data_to_html_data(pdf_data, datatype='pdf')
+            pdf_data = zip_root["Details.pdf"].read("rb")
+            pdf_data = data_to_html_data(pdf_data, datatype="pdf")
         else:
             pdf_data = None
 
         return {
-            'figure_data': figure_data,
-            'digestions': selected_digestions,
-            'score': score,
-            'pdf_data': pdf_data
+            "figure_data": figure_data,
+            "digestions": selected_digestions,
+            "score": score,
+            "pdf_data": pdf_data,
         }
+
 
 class SelectDigestionsView(StartJobView):
     serializer_class = serializer_class
