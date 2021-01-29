399a8f599f240c08d6dbb841fd89f8f737a01038
Peter Vegh <pvegh@posteo.uk>
2021-01-29 20:55:35 +0000

    Format to Black

--- backend/app/views/analyze_digests/AnalyzeDigests.py
+++ backend/app/views/analyze_digests/AnalyzeDigests.py
@@ -4,11 +4,14 @@ from collections import OrderedDict
 
 from rest_framework import serializers
 from ..base import AsyncWorker, StartJobView
-from ..tools import (records_from_data_files, data_to_html_data,
-                     file_to_filelike_object,
-                     did_you_mean,
-                     set_record_topology,
-                     matplotlib_figure_to_svg_base64_data)
+from ..tools import (
+    records_from_data_files,
+    data_to_html_data,
+    file_to_filelike_object,
+    did_you_mean,
+    set_record_topology,
+    matplotlib_figure_to_svg_base64_data,
+)
 from ..serializers import FileSerializer
 
 import flametree
@@ -36,11 +39,12 @@ class serializer_class(serializers.Serializer):
     subanalysis = serializers.CharField()
     topology = serializers.CharField()
 
+
 def file_type(f):
-    return 'csv' if f.name.lower().endswith('csv') else 'excel'
+    return "csv" if f.name.lower().endswith("csv") else "excel"
+
 
 class worker_class(AsyncWorker):
-
     def work(self):
         self.logger(message="Reading the files...")
         data = self.data
@@ -56,44 +60,45 @@ class worker_class(AsyncWorker):
         constructs_plate = plate_from_platemap_spreadsheet(
             file_to_filelike_object(data.constructsMap),
             file_type=file_type(data.constructsMap),
-            data_field='construct', headers=True)
-        constructs_map = OrderedDict([
-            (well.name, well.data.construct)
-            for well in constructs_plate.iter_wells(direction='row')
-            if 'construct' in well.data
-            and str(well.data.construct) != 'nan'
-        ])
+            data_field="construct",
+            headers=True,
+        )
+        constructs_map = OrderedDict(
+            [
+                (well.name, well.data.construct)
+                for well in constructs_plate.iter_wells(direction="row")
+                if "construct" in well.data and str(well.data.construct) != "nan"
+            ]
+        )
         unknown_constructs = {}
         for well, construct in constructs_map.items():
             if construct not in constructs_records:
                 unknown_constructs[construct] = {
                     "well": well,
-                    "suggestions": did_you_mean(construct, constructs_records)
+                    "suggestions": did_you_mean(construct, constructs_records),
                 }
         if len(unknown_constructs):
-            return {
-                "success": False,
-                "unknown_constructs": unknown_constructs
-            }
-
+            return {"success": False, "unknown_constructs": unknown_constructs}
 
         if data.uniqueDigestion:
             digestion = tuple(data.digestion)
-            digestions_map = OrderedDict([
-                (wellname, digestion)
-                for wellname in constructs_map
-            ])
+            digestions_map = OrderedDict(
+                [(wellname, digestion) for wellname in constructs_map]
+            )
         else:
             digestions_plate = plate_from_platemap_spreadsheet(
                 file_to_filelike_object(data.digestionsMap),
                 file_type=file_type(data.digestionsMap),
-                data_field='digestion', headers=True)
-            digestions_map = OrderedDict([
-                (well.name, tuple(well.data.digestion.split(', ')))
-                for well in digestions_plate.iter_wells(direction='row')
-                if 'digestion' in well.data
-                and str(well.data.digestion) != 'nan'
-            ])
+                data_field="digestion",
+                headers=True,
+            )
+            digestions_map = OrderedDict(
+                [
+                    (well.name, tuple(well.data.digestion.split(", ")))
+                    for well in digestions_plate.iter_wells(direction="row")
+                    if "digestion" in well.data and str(well.data.digestion) != "nan"
+                ]
+            )
 
         archive = file_to_filelike_object(data.fragmentAnalysisArchive)
 
@@ -102,97 +107,105 @@ class worker_class(AsyncWorker):
         self.logger(message="Analyzing the data...")
 
         observations = BandsObservation.from_aati_fa_archive(
-            archive, ignore_bands_under=data.ignoreBandsUnder)
-        clones = Clone.from_bands_observations(observations, constructs_map,
-                                               digestions_map)
+            archive, ignore_bands_under=data.ignoreBandsUnder
+        )
+        clones = Clone.from_bands_observations(
+            observations, constructs_map, digestions_map
+        )
         clones_observations = ClonesObservations(clones, constructs_records)
-        if data.subanalysis == 'partial_digests':
+        if data.subanalysis == "partial_digests":
             return self.partial_digests_analysis(data, clones_observations)
         else:
             return self.validation_analysis(data, clones_observations)
 
     def partial_digests_analysis(self, data, clones_observations):
         analysis = clones_observations.partial_digests_analysis()
-        best = max(analysis, key=lambda a: analysis[a]['valid_clones'])
-        if analysis[best]['valid_clones'] == analysis[()]['valid_clones']:
+        best = max(analysis, key=lambda a: analysis[a]["valid_clones"])
+        if analysis[best]["valid_clones"] == analysis[()]["valid_clones"]:
             return {
-              'message': 'The partial digest analysis did not find any '
-                         'significant results',
-              'success': 'yeah!'
+                "message": "The partial digest analysis did not find any "
+                "significant results",
+                "success": "yeah!",
             }
         ax = clones_observations.plot_partial_digests_analysis(analysis)
-        validations = analysis[best]['validations']
+        validations = analysis[best]["validations"]
         observations = ClonesObservations(
             clones_observations.clones,
             clones_observations.constructs_records,
-            partial_cutters=best
+            partial_cutters=best,
         )
         pdf_data = observations.plot_all_validations_patterns(validations)
         return {
-            'pdf_file': {
-                'data': data_to_html_data(pdf_data, 'pdf'),
-                'name': 'digest_validation_assuming_partial_%s.pdf' %
-                        "_".join(best),
-                'mimetype': 'application/pdf'
+            "pdf_file": {
+                "data": data_to_html_data(pdf_data, "pdf"),
+                "name": "digest_validation_assuming_partial_%s.pdf" % "_".join(best),
+                "mimetype": "application/pdf",
             },
-            'message': (
+            "message": (
                 "The analysis shows that the following enzyme(s) were"
                 " possibly only partially cutting: %s. <br/>The report below"
                 " shows the validation under this hypothesis."
-            ) % (", ".join(["<b>%s</b>" % b for b in best])),
-            'figure_data':  matplotlib_figure_to_svg_base64_data(
-                              ax.figure, bbox_inches='tight'),
-            'success': 'yeah!'
+            )
+            % (", ".join(["<b>%s</b>" % b for b in best])),
+            "figure_data": matplotlib_figure_to_svg_base64_data(
+                ax.figure, bbox_inches="tight"
+            ),
+            "success": "yeah!",
         }
 
     def validation_analysis(self, data, clones_observations):
         validations = clones_observations.validate_all_clones(
             min_band_cutoff=data.bandsRange[0],
             max_band_cutoff=data.bandsRange[1],
-            relative_tolerance=data.tolerance
+            relative_tolerance=data.tolerance,
         )
         first_validation = list(validations.values())[0]
         ladder = list(first_validation.clone.digestions.values())[0].ladder
 
         # CREATE A ZIP WITH VALIDATION REPORTS
 
-        zip_root = flametree.file_tree('@memory')
+        zip_root = flametree.file_tree("@memory")
         self.logger(message="Generating the validation report...")
-        zip_root._file('validations.pdf').write(
+        zip_root._file("validations.pdf").write(
             clones_observations.plot_all_validations_patterns(validations)
         )
         clones_observations.validations_summary_table(
-            validations, zip_root._file('summary.csv').open('w'))
+            validations, zip_root._file("summary.csv").open("w")
+        )
         if data.includeDigestionPlots:
             self.logger(message="Plotting cuts maps...")
             co = clones_observations
 
             plot_records_digestions(
-                target=zip_root._file('digestions.pdf').open('wb'),
+                target=zip_root._file("digestions.pdf").open("wb"),
                 ladder=ladder,
                 records_and_digestions=[
                     (co.constructs_records[cst], digestion_)
                     for cst, digestions in co.constructs_digestions.items()
                     for digestion_ in digestions
-                ]
+                ],
             )
             # zip_root._file('digestions.pdf').write(pdf_data)
 
         self.logger(message="Generating the success plate map...")
         ax = clones_observations.plot_validations_plate_map(validations)
-        ax.figure.savefig(zip_root._file('success_map.pdf').open('wb'),
-                          format='pdf', bbox_inches='tight')
+        ax.figure.savefig(
+            zip_root._file("success_map.pdf").open("wb"),
+            format="pdf",
+            bbox_inches="tight",
+        )
 
         self.logger(message="All done !")
 
         return {
-          'zip_file': {
-              'data': data_to_html_data(zip_root._close(), 'zip'),
-              'name': 'validation_report.zip',
-              'mimetype': 'application/zip'
-          },
-          'success': 'yeah!'
+            "zip_file": {
+                "data": data_to_html_data(zip_root._close(), "zip"),
+                "name": "validation_report.zip",
+                "mimetype": "application/zip",
+            },
+            "success": "yeah!",
         }
+
 
 class AnalyzeDigestsView(StartJobView):
     serializer_class = serializer_class
